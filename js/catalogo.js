5140fe8239f9830edf4ae2d19b9cc48b1d275e5c
Luis.Aguilar <aguilar.luiseder27@gmail.com>
2026-06-04 14:37:17 -0600

    desarrolle catalogo, tanto el html como css y el js

---
+++ js/catalogo.js
@@ -0,0 +1,40 @@
+const xhttp = new XMLHttpRequest();
+console.log(xhttp);
+xhttp.onload = function () {
+  const xmlDoc = xhttp.responseXML;
+  const libro = xmlDoc.getElementsByTagName("libro");
+  const autors = xmlDoc.getElementsByTagName("autor");
+  console.log(libro);
+  console.log(autors);
+  mostrarLibros(libro, autors);
+};
+xhttp.open("GET", "../DB/biblioteca.xml");
+xhttp.send();
+
+const repisa = document.getElementsByClassName("repisa")[0];
+
+function mostrarLibros(libro, autors) {
+  let html = "";
+
+  //console.log(libro);
+
+  for (let i = 0; i < 29; i++) {
+    const titulo = libro[i].getElementsByTagName("titulo")[0].textContent;
+    //console.log(titulo);
+    const autor = autors[i].getElementsByTagName("nombre")[0].textContent;
+
+    html += `
+            <div class="libro">
+              <div class="portada">
+                <img src="/img/book.png"
+                     alt="Portada"
+                     width="80">
+              </div>
+
+                <h3>${titulo}</h3>
+                <p>${autor}</p>
+            </div>
+        `;
+  }
+  repisa.innerHTML = html;
+}
