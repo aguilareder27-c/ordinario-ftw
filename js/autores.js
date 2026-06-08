3d08815c3e3382b92ce3df7b0782bb6b70659cde
Luis.Aguilar <aguilar.luiseder27@gmail.com>
2026-06-08 05:24:21 -0600

    filtros en autores y favoritos

--- js/autores.js
+++ js/autores.js
@@ -3,15 +3,37 @@ console.log(xhttp);
 xhttp.onload = function () {
   const xmlDoc = xhttp.responseXML;
   const autors = xmlDoc.getElementsByTagName("autor");
+  llenarNacionalidades(autors);
   console.log(autors);
-  mostrarLibros(autors);
+  mostrarLibros(autors, "todas");
+
+  const filtro = document.getElementById("filtroNacionalidad");
+  filtro.addEventListener("change", function () {
+    mostrarLibros(autors, filtro.value);
+  });
 };
 xhttp.open("GET", "../DB/biblioteca.xml");
 xhttp.send();
 
 const repisa = document.getElementsByClassName("repisa")[0];
 
-function mostrarLibros(autors) {
+function llenarNacionalidades(autors) {
+  const filtro = document.getElementById("filtroNacionalidad");
+  const nacionalidades = [];
+  for (let i = 0; i < autors.length; i++) {
+    const nac = autors[i].getElementsByTagName("nacionalidad")[0].textContent;
+    if (nacionalidades.indexOf(nac) === -1) {
+      nacionalidades.push(nac);
+    }
+  }
+  for (let i = 0; i < nacionalidades.length; i++) {
+    const opcion = document.createElement("option");
+    opcion.value = nacionalidades[i];
+    opcion.textContent = nacionalidades[i];
+    filtro.appendChild(opcion);
+  }
+}
+function mostrarLibros(autors, nacionalidad) {
   let html = "";
 
   //console.log(libro);
@@ -19,8 +41,11 @@ function mostrarLibros(autors) {
   for (let i = 0; i < autors.length; i++) {
     //console.log(titulo);
     const autor = autors[i].getElementsByTagName("nombre")[0].textContent;
-    const nacionalidad =
-      autors[i].getElementsByTagName("nacionalidad")[0].textContent;
+    const nac = autors[i].getElementsByTagName("nacionalidad")[0].textContent;
+
+    if (nacionalidad !== "todas" && nac !== nacionalidad) {
+      continue;
+    }
 
     html += `
             <div class="libro">
@@ -30,7 +55,7 @@ function mostrarLibros(autors) {
                      width="80">
               </div>
                 <p>${autor}</p>
-                <p class="nacionalidad">${nacionalidad}</p>
+                <p class="nacionalidad">${nac}</p>
             </div>
         `;
   }
--- js/favoritos.js
+++ js/favoritos.js
@@ -4,27 +4,62 @@ xhttp.onload = function () {
   const xmlDoc = xhttp.responseXML;
   const libro = xmlDoc.getElementsByTagName("libro");
   const fav = xmlDoc.getElementsByTagName("favorito");
+  llenarGeneros(libro, fav);
   console.log(libro);
   console.log(fav);
-  mostrarLibros(libro, fav);
+  mostrarLibros(libro, fav, "todos");
+
+  const filtro = document.getElementById("filtroGenero");
+  filtro.addEventListener("change", function () {
+    mostrarLibros(libro, fav, filtro.value);
+  });
 };
 xhttp.open("GET", "../DB/biblioteca.xml");
 xhttp.send();
 
 const repisa = document.getElementsByClassName("repisa")[0];
 
-function mostrarLibros(libro, fav) {
+function esFavorito(fav, id) {
+  for (let j = 0; j < fav.length; j++) {
+    if (fav[j].getAttribute("libro") === id) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function llenarGeneros(libro, fav) {
+  const filtro = document.getElementById("filtroGenero");
+  const generos = [];
+  for (let i = 0; i < libro.length; i++) {
+    const id = libro[i].getAttribute("id");
+    if (!esFavorito(fav, id)) continue;
+    const gen = libro[i].getElementsByTagName("genero")[0].textContent;
+    if (generos.indexOf(gen) === -1) {
+      generos.push(gen);
+    }
+  }
+  for (let i = 0; i < generos.length; i++) {
+    const opcion = document.createElement("option");
+    opcion.value = generos[i];
+    opcion.textContent = generos[i];
+    filtro.appendChild(opcion);
+  }
+}
+
+function mostrarLibros(libro, fav, genero) {
   let html = "";
 
-  //console.log(libro);
-
   for (let i = 0; i < libro.length; i++) {
-    const titulo = libro[i].getElementsByTagName("titulo")[0].textContent;
     const id = libro[i].getAttribute("id");
+    if (!esFavorito(fav, id)) continue;
 
-    for (let j = 0; j < fav.length; j++) {
-      if (fav[j].getAttribute("libro") === id) {
-        html += `
+    const titulo = libro[i].getElementsByTagName("titulo")[0].textContent;
+    const gen = libro[i].getElementsByTagName("genero")[0].textContent;
+
+    if (genero !== "todos" && gen !== genero) continue;
+
+    html += `
             <div class="libro">
               <div class="portada">
                 <img src="../img/book.png"
@@ -35,8 +70,6 @@ function mostrarLibros(libro, fav) {
                 <h3>${titulo}</h3>
             </div>
         `;
-      }
-    }
   }
 
   repisa.innerHTML = html;
